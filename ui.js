dccbbedd71b56b128ead49ad83e88b2a8f051f88
Nathan Gomes <nathansoftwaredev@gmail.com>
2022-10-23 21:19:36 -0400

    Fetch user repos and display in UI

--- ui.js
+++ ui.js
@@ -1,8 +1,11 @@
 class UI {
-    constructor() {
-      this.profile = document.getElementById('profile')
-    }
+  constructor() {
+    this.profile = document.getElementById('profile')
+  }
   
+
+
+  // Display profile in UI
   showProfile(user) {
     this.profile.innerHTML = `
       <div class="card card-body mb-3">
@@ -31,39 +34,68 @@ class UI {
     `
   }
 
+
+
+  // Show repositorites
+  showRepos(repos) {
+    let output = ''
+
+    repos.forEach(function(repo) {
+      output += `
+      <div class="card card-body mb-2">
+        <div class="row">
+          <div class="col-md-6">
+            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+          </div>
+          <div class="col-md-6">
+            <span class="badge badge-primary">Stars: ${repo.stargazers_count}</span>
+            <span class="badge badge-secondary">Watchers: ${repo.watchers_count}</span>
+            <span class="badge badge-success">Forks: ${repo.forms_count}</span>
+          </div>
+        </div>
+      </div>
+      `
+    })
+
+    // Output repos
+    document.getElementById('repos').innerHTML = output
+  }
+
+
+
   // Show alert message
   showAlert(message, className) {
-  // Clear any remaining alerts
-  this.clearAlert()
-  // Create div
-  const div  =  document.createElement('div')
-
-  div.className = className
-  // Add text
-  div.appendChild(document.createTextNode(message))
-  // Get parent
-  const container =  document.querySelector('.searchContainer')
-  // Get search box
-  const search = document.querySelector('.search')
-  // Insert alert
-  container.insertBefore(div, search)
-
-  setTimeout(() => {
+    // Clear any remaining alerts
     this.clearAlert()
-  }, 3000)
+    // Create div
+    const div  =  document.createElement('div')
+
+    div.className = className
+    // Add text
+    div.appendChild(document.createTextNode(message))
+    // Get parent
+    const container =  document.querySelector('.searchContainer')
+    // Get search box
+    const search = document.querySelector('.search')
+    // Insert alert
+    container.insertBefore(div, search)
+
+    setTimeout(() => {
+      this.clearAlert()
+    }, 3000)
   }
 
   // Clear alert message
   clearAlert() {
-  const currentAlert = document.querySelector('.alert')
+    const currentAlert = document.querySelector('.alert')
 
-  if(currentAlert){
-    currentAlert.remove()
-  }
+    if(currentAlert){
+      currentAlert.remove()
+    }
   }
 
   // Clear profile
   clearProfile() {
-  this.profile.innerHTML = ''
+    this.profile.innerHTML = ''
   }
 }
